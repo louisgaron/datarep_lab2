a7bee0708bca28f2d288b5864a8d041774cb3be8
louisgaron <g00346194@gmit.ie>
2020-10-08 09:30:16 +0100

    First Commit

---
+++ cars.js
@@ -0,0 +1,37 @@
+ class Vehicle
+ {
+     constructor(make, model, year)
+     {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+     }
+
+     Information()
+     {
+         console.log(`Make: ${this.make}    Model: ${this.model}    Year: ${this.year}`);
+
+     }
+ }
+
+ //let myVehicle = new Vehicle('VW', 'Gold', 2008);
+ //let info = myVehicle.Information();
+
+ class Cars extends Vehicle
+ {
+     constructor(make, model, year, doors)
+     {
+        super(make, model, year);
+        this.doors = doors;
+     }
+
+     Information()
+     {
+         super.Information();
+         console.log(`Doors: ${this.doors}`);
+
+     }
+ }
+
+ let myCar = new Cars('Audi', 'A4', 2009, 4);
+ myCar.Information();
